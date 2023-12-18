0e8b2b31cab3e60387c5cbf0f88de8725d9706fd
Dmitriy Barvinok <barvinok_dima@mail.ru>
2023-12-18 16:23:26 +0600

    force

--- views/chat_views.py
+++ views/chat_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, session, render_template, redirect, request
+from flask import Blueprint, session, render_template, redirect, request, jsonify
 from utils.db_manager import get_session
 from classes.users import Users
 from classes.friends import Friends
@@ -13,7 +13,7 @@ Session = get_session()
 
 
 # Маршрут для чата
-@chat_bp.route('/chat', methods=['GET', 'POST'])
+@chat_bp.route('/chat', methods=['GET'])
 def chat():
 	# Проверяем авторизован ли пользователь
 	user_id = session.get('user_id')
@@ -29,47 +29,116 @@ def chat():
 	if chat_id == None:
 		return render_template('chat.html', user=user)
 	
-	# Проверяем, существует ли чат, где участником является пользователь
+	# Проверяем, существует ли чат
 	with Session as SQLSession:
-		chat = SQLSession.query(Chats).join(ChatMembers).filter(ChatMembers.user_id == user_id).filter(Chats.chat_id == chat_id).first()
-		print(chat)
-		if not chat:
-			return redirect('/chat')
-		
-		# Получаем последние 20 сообщений
-		messages = SQLSession.query(Messages).filter(Messages.chat_id == chat_id).order_by(Messages.message_id.desc()).limit(20).all()
-		messages.reverse()
+		chat = SQLSession.query(Chats).filter(Chats.chat_id == chat_id).first()
+	if not chat:
+		return redirect('/chat')
 
-		return render_template('chat.html', user=user, chat=chat, messages=messages)
+	# Проверяем, является ли пользователь участником чата
+	with Session as SQLSession:
+		chat_member = SQLSession.query(ChatMembers).filter(ChatMembers.chat_id == chat_id).filter(ChatMembers.user_id == user_id).first()
+	if not chat_member:
+		return redirect('/chat')
+	
+	# Получаем 20 последних сообщений
+	with Session as SQLSession:
+		messages = SQLSession.query(Messages).filter(Messages.chat_id == chat_id).order_by(Messages.timestamp.desc()).limit(20).all()
+	print('Получили 20 последних сообщений')
+
+	# Получаем участников чата
+	with Session as SQLSession:
+		chat_members = SQLSession.query(ChatMembers).filter(ChatMembers.chat_id == chat_id).all()
+	print('Получили участников чата')
+
+	# Возвращаем страницу чата
+	return render_template('chat.html', user=user, chat=chat, messages=messages, chat_members=chat_members)
 
 
+# # Маршрут для получения сообщений
+# @chat_bp.route('/get_messages', methods=['POST', 'GET'])
+# def get_messages():
+# 	# Проверяем авторизован ли пользователь
+# 	user_id = session.get('user_id')
+# 	if not user_id:
+# 		return redirect('/login')
+	
+# 	# Получаем данные пользователя
+# 	with Session as SQLSession:
+# 		user = SQLSession.query(Users).filter(Users.user_id == user_id).first()
+
+# 	# Проверяем, выбран ли чат
+# 	chat_id = request.args.get('id')
+# 	if chat_id == None:
+# 		return render_template('chat.html', user=user)
+	
+# 	# Проверяем, существует ли чат
+# 	with Session as SQLSession:
+# 		chat = SQLSession.query(Chats).filter(Chats.chat_id == chat_id).first()
+# 	if not chat:
+# 		return redirect('/chat')
+
+# 	# Проверяем, является ли пользователь участником чата
+# 	with Session as SQLSession:
+# 		chat_member = SQLSession.query(ChatMembers).filter(ChatMembers.chat_id == chat_id).filter(ChatMembers.user_id == user_id).first()
+# 	if not chat_member:
+# 		return redirect('/chat')
+	
+# 	# Получаем 20 последних сообщений
+# 	with Session as SQLSession:
+# 		messages = SQLSession.query(Messages).filter(Messages.chat_id == chat_id).order_by(Messages.timestamp.desc()).limit(20).all()
+# 	print('Получили 20 последних сообщений')
+
+# 	# Получаем участников чата
+# 	with Session as SQLSession:
+# 		chat_members = SQLSession.query(ChatMembers).filter(ChatMembers.chat_id == chat_id).all()
+# 	print('Получили участников чата')
+
+# 	# Возвращаем страницу чата
+# 	# post запрос
+# 	# if request.method == 'POST':
+# 	# Пакуем данные в json
+# 	data = {
+# 		'messags': messages,
+# 		'chat_members': chat_members
+# 	}
+# 	# Возвращаем json
+# 	print('data', data)
+# 	return jsonify(data)
 
 
-
-# Маршрут для создания чата
-@chat_bp.route('/create_chat', methods=['POST'])
-def create_chat():
+# Маршрут для чата
+@chat_bp.route('/personal_messages', methods=['POST'])
+def personal_messages():
 	# Проверяем авторизован ли пользователь
 	user_id = session.get('user_id')
 	if not user_id:
 		return redirect('/login')
 	print('user_id', user_id)
 
-	# Проверяем, существует ли чат, где участником является пользователь
+	# Проверяем, существует ли чат, где участником является наш собеседник
 	target_id = request.form.get('user_id')
 	print('target_id', target_id)
 	with Session as SQLSession:
-		# Проверяем, существует ли чат, где участником является пользователь
-		chat = (
-			SQLSession.query(Chats)
-			.join(ChatMembers, Chats.chat_id == ChatMembers.chat_id)
-			.filter(ChatMembers.user_id == user_id)
-			.filter(ChatMembers.user_id == target_id)
-			.first()
-		)
-		print('chat', chat)
-		if chat:
-			return redirect('/chat?id=' + str(chat.chat_id))
+		# Ищем все чаты текущего пользователя
+		chats = SQLSession.query(ChatMembers).filter(ChatMembers.user_id == user_id).all()
+		# Получаем имя и фамилию собеседника
+		target = SQLSession.query(Users).filter(Users.user_id == target_id).first()
+		for chat in chats:
+			tmp = SQLSession.query(ChatMembers).filter(ChatMembers.chat_id == chat.chat_id).filter(ChatMembers.user_id == target_id).first()
+			# Когда нашли чат, где участником является наш собеседник
+			if tmp:
+				# Получаем 20 последних сообщений
+				messages = SQLSession.query(Messages).filter(Messages.chat_id == chat.chat_id).order_by(Messages.timestamp.desc()).limit(20).all()
+				# Пакуем данные в json
+				data = {
+					'messages': messages,
+					'chat_id': chat.chat_id,
+					'chat_name': target.name + ' ' + target.surname
+				}
+				# Возвращаем json
+				print('data', data)
+				return jsonify(data)
 	
 	# Создаем чат
 	with Session as SQLSession:
@@ -92,6 +161,29 @@ def create_chat():
 		chat_member.joined_at = datetime.now()
 		SQLSession.add(chat_member)
 		SQLSession.commit()
-		
-	print('CHAT ID:', str(chat.chat_id))
-	return redirect('/chat?id=' + str(chat.chat_id))
+
+	with Session as SQLSession:
+		# Ищем все чаты текущего пользователя
+		chats = SQLSession.query(ChatMembers).filter(ChatMembers.user_id == user_id).all()
+		# Получаем имя и фамилию собеседника
+		target = SQLSession.query(Users).filter(Users.user_id == target_id).first()
+		for chat in chats:
+			tmp = SQLSession.query(ChatMembers).filter(ChatMembers.chat_id == chat.chat_id).filter(ChatMembers.user_id == target_id).first()
+			# Когда нашли чат, где участником является наш собеседник
+			if tmp:
+				# Получаем 20 последних сообщений
+				messages = SQLSession.query(Messages).filter(Messages.chat_id == chat.chat_id).order_by(Messages.timestamp.desc()).limit(20).all()
+				# Пакуем данные в json
+				data = {
+					'user_id': user_id,
+					'target_id': target_id,
+					'chat_id': chat.chat_id,
+					'chat_name': target.name + ' ' + target.surname,
+					'messages': messages,
+				}
+				# Возвращаем json
+				print('data', data)
+				return jsonify(data)
+			
+	# Если не нашли чат, то возвращаем ошибку
+	return jsonify({'error': 'Чат не найден'})
